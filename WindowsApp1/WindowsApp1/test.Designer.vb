0e61608ecd4061d8dbe9219c58c3a7bf2dd4888d
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2020-02-10 14:07:20 +0700

    wow

---
+++ WindowsApp1/WindowsApp1/test.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class test
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.showcate = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.dgvnorth = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvnorth, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'showcate
+        '
+        Me.showcate.Location = New System.Drawing.Point(12, 12)
+        Me.showcate.Name = "showcate"
+        Me.showcate.Size = New System.Drawing.Size(140, 91)
+        Me.showcate.TabIndex = 0
+        Me.showcate.Text = "Button1"
+        Me.showcate.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(158, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(140, 91)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'dgvnorth
+        '
+        Me.dgvnorth.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvnorth.Location = New System.Drawing.Point(12, 109)
+        Me.dgvnorth.Name = "dgvnorth"
+        Me.dgvnorth.RowHeadersWidth = 51
+        Me.dgvnorth.RowTemplate.Height = 24
+        Me.dgvnorth.Size = New System.Drawing.Size(742, 329)
+        Me.dgvnorth.TabIndex = 2
+        '
+        'test
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dgvnorth)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.showcate)
+        Me.Name = "test"
+        Me.Text = "test"
+        CType(Me.dgvnorth, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents showcate As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents dgvnorth As DataGridView
+End Class
---
+++ WindowsApp1/WindowsApp1/test.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+Imports System.Configuration
+Public Class test
+    Dim myCon As New SqlConnection
+    Dim myDa As New SqlDataAdapter
+    Dim myDs As New DataSet
+
+    Dim strCon As String = ConfigurationManager.ConnectionStrings("WindowsApp1.My.MySettings.sqlline").ConnectionString
+    Dim strSQL As String
+
+    Private Sub connectDB()
+        If myCon.State = ConnectionState.Open Then
+            myCon.Close()
+        End If
+        myCon.ConnectionString = strCon
+        myCon.Open()
+    End Sub
+
+    Private Sub showcate_Click(sender As Object, e As EventArgs) Handles showcate.Click
+        connectDB()
+        strSQL = "Select * from Categories"
+        myDa = New SqlDataAdapter(strSQL, myCon)
+        myDa.Fill(myDs, "tbcat")
+        dgvnorth.DataSource = myDs.Tables("tbcat")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        connectDB()
+        strSQL = "Select ProductID,ProductName,QuantityPerUnit,UnitPrice,UnitsInStock from Products"
+        myDa = New SqlDataAdapter(strSQL, myCon)
+        myDa.Fill(myDs, "tbcat")
+        dgvnorth.DataSource = myDs.Tables("tbcat")
+    End Sub
+End Class
